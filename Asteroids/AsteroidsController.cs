4096a829e37920a720d64da30d6a4df5e7688506
Monika Malolepsza <malolepszamonika@gmail.com>
2022-05-15 10:07:37 +0200

    Add project files.

---
+++ Asteroids/AsteroidsController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Asteroids
+{
+    public class AsteroidsController
+    {
+        private readonly byte[] _command = { (byte)'c', (byte)'t', (byte)'m', (byte)'a', (byte)'m', (byte)'e', (byte)'@', (byte)'0' };
+        private string _adress;
+        private int _port;
+
+        public AsteroidsController()
+        {
+            _adress = "127.0.0.1";
+            _port = 1979;
+        }
+
+        public AsteroidsController(string adress, int port)
+        {
+            _adress = adress;
+            _port = port;
+        }
+
+        public byte[] SendCommand(int cmd)
+        {
+            _command[6] = MapCommand(cmd);
+            using (UdpClient client = new UdpClient(_adress, _port))
+            {
+                client.Send(_command, 8);
+                if (cmd == 5 || cmd == 3)
+                {
+                    Thread.Sleep(10);
+                    _command[6] = MapCommand(0);
+                    client.Send(_command, 8);
+                }
+
+                var b = client.ReceiveAsync().Result.Buffer;
+                return b;
+            }
+        }
+
+        private byte MapCommand(int cmd)
+        {
+            switch (cmd)
+            {
+                case 0:
+                    return (byte)0x40;
+
+                case 1:
+                    return (byte)0x50;
+
+                case 2:
+                    return (byte)0x48;
+
+                case 3:
+                    return (byte)0x42;
+
+                case 4:
+                    return (byte)0x44;
+
+                case 5:
+                    return (byte)0x41;
+            }
+            throw new Exception($"Invalid command {cmd}");
+        }
+    }
+}
---
+++ Asteroids/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Asteroids
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var controller = new AsteroidsController();
+
+            bool isRunning = true;
+
+            while (isRunning)
+            {
+                var key = Console.ReadKey(true);
+                switch (key.Key)
+                {
+                    case ConsoleKey.W:
+                        controller.SendCommand(4);
+                        break;
+
+                    case ConsoleKey.A:
+                        controller.SendCommand(1);
+                        break;
+
+                    case ConsoleKey.D:
+                        controller.SendCommand(2);
+                        break;
+
+                    case ConsoleKey.P:
+                        controller.SendCommand(3);
+                        break;
+
+                    case ConsoleKey.Spacebar:
+                        controller.SendCommand(5);
+                        break;
+
+                    case ConsoleKey.Escape:
+                        isRunning = false;
+                        break;
+                }
+            }
+        }
+    }
+}
